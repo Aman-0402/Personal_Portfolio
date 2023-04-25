7487194cbfce480157483b4569c6af1061d78c2f
Aman Raj <114799017+Aman-0402@users.noreply.github.com>
2023-04-25 15:26:15 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hello, Myself Aman Raj.</span><br>I am Student of Sambalpur University Institute of Information Technology(SUIIT) pursing B.Tech Computer science. Currently I'm in 6th Semester.<br>I'm very dediicated to my work. <br> I'm Web developer and Java Developer. "
+      ],
+      typeSpeed: 10,
+      contentType: 'html'
+    });
+  }, 100);
